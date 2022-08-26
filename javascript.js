f0ee7bba530bc7e87a4783ddfbab867932bec778
EsmeraldaTorres <estorresag2019@gmail.com>
2022-08-25 23:06:46 -0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,69 @@
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play");
+
+function abrir() {
+  document.getElementById("sobre-arriba").classList.add("animate__fadeOutUp");
+  document.getElementById("sobre-abajo").classList.add("animate__slideOutDown");
+  document.getElementById("btn-open").classList.remove("animate__infinite");
+  document.getElementById("btn-open").classList.add("animate__fadeOut");
+  setTimeout(function () {
+    document.getElementById("hoja-principal").classList.add("hide");
+    document.getElementById("invitacion").classList.remove("hide");
+    audio.play();
+  }, 3000);
+}
+
+// auidio
+
+playPause.addEventListener("click", () => {
+  if (audio.paused || audio.ended) {
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+    audio.play();
+  } else {
+    audio.pause();
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+  }
+});
+
+// Cuenta regresiva -------------------------------------------------------------------
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 20, 2022 20:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = `
+  <div class="d-flex justify-content-center">
+    <div class="cuenta p-4" >
+  <span>${days} días</span>
+  <span class="ml-2 mr-2"> : </span>  
+  <span>${hours} hrs</span> 
+  <span class="ml-2 mr-2"> : </span>   
+  <span>${minutes} min</span>
+  <span class="ml-2 mr-2"> : </span>  
+  <span>${seconds} seg</span> 
+    </div>
+  </div>
+    `;
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+// carousel
